de869a8c712923a774ef239391dc21ae536bc182
Yanina Aular <yanina.aular@vauxoo.com>
2013-04-03 15:32:52 -0430

    [ADD] paquetes 32 y 64 divididos, descargar con wget

---
+++ install.sh
@@ -0,0 +1,137 @@
+﻿#!/bin/bash
+
+USUARIO=yanina
+distro=32
+
+echo ""
+echo "---------------------------------------------------------"
+echo "\nActualizando repositorios\n"
+echo "---------------------------------------------------------"
+echo ""
+
+apt-get -y update
+
+echo ""
+echo "---------------------------------------------------------"
+echo "Actualizar versiones repositorios..."
+echo "---------------------------------------------------------"
+echo ""
+
+apt-get -y upgrade
+
+echo ""
+echo "---------------------------------------------------------"
+echo "Instalando paquetes..."
+echo "---------------------------------------------------------"
+echo ""
+
+apt-get install -y \
+yakuake \
+bzr \
+meld \
+vlc \
+geany \
+openssh-client \
+texlive-full \
+texmaker \
+qbzr \
+bzr-explorer \
+bzrtools \
+gimp \
+geany-plugins \
+aptitude \
+kazam \
+unetbootin \
+python3-pip \
+python-pip \
+pep8 \
+vim \
+virtualbox \
+pgadmin3 \
+make \
+doxygen \
+unrar \
+unetbootin \
+rst2pdf \
+google-chrome-stable \
+google-talkplugin \
+
+echo "---------------------------------------------------------"
+echo "\nInstalando autopep8...\n"
+pip install autopep8
+echo "---------------------------------------------------------"
+
+
+
+echo ""
+echo "---------------------------------------------------------"
+echo "Instalando Sphinx"
+echo "---------------------------------------------------------"
+echo ""
+easy_install -U Sphinx
+
+wget -i -c $distro.txt
+echo "\nInstalando paquetes descargados...\n"
+dpkg -i *.deb
+
+apt-get install -y -f
+echo "---------------------------------------------------------"
+
+
+
+apt-get install -y \
+graphviz \
+ghostscript \
+postgresql-client \
+python-dateutil \
+python-feedparser python-gdata \
+python-ldap \
+python-libxslt1 \
+python-lxml \
+python-mako \
+python-openid \
+python-psycopg2 \
+python-pybabel \
+python-pychart \
+python-pydot \
+python-pyparsing \
+python-reportlab \
+python-simplejson \
+python-tz \
+python-vatnumber \
+python-vobject \
+python-webdav \
+python-werkzeug \
+python-xlwt \
+python-yaml \
+python-imaging \
+python-matplotlib
+
+apt-get install -y python-docutils
+easy_install mock
+easy_install unittest2
+easy_install psutil 
+easy_install 
+
+apt-get install -y -f
+
+exit
+#mkdir /home/$USUARIO/instancias
+#mkdir /home/$USUARIO/instancias/estable
+#mkdir /home/$USUARIO/instancias/estable/7.0
+#mkdir /home/$USUARIO/instancias/estable/7.0/server
+#mkdir /home/$USUARIO/instancias/estable/7.0/addons
+#mkdir /home/$USUARIO/instancias/estable/7.0/web
+#mkdir /home/$USUARIO/instancias/estable/6.1
+#mkdir /home/$USUARIO/instancias/estable/6.1/server
+#mkdir /home/$USUARIO/instancias/estable/6.1/addons
+#mkdir /home/$USUARIO/instancias/estable/6.1/web
+#mkdir /home/$USUARIO/instancias/estable/6.1/client
+#mkdir /home/$USUARIO/bzr_projects
+#mkdir /home/$USUARIO/bzr_projects/_VE
+#mkdir /home/$USUARIO/bzr_projects/_VE/ovl_branches
+#mkdir /home/$USUARIO/bzr_projects/_VE/av_branches
+#mkdir /home/$USUARIO/bzr_projects/_VE/ott_branches
+#mkdir /home/$USUARIO/bzr_projects/_VE/doc_branches
+#mkdir /home/$USUARIO/bzr_projects/oerplib
+#mkdir /home/$USUARIO/bzr_projects/openobject-doc
